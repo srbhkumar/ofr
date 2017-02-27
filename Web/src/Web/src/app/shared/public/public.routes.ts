7d747b9862b2de723bc3b6dd38f311b082c60de2
Ramya Jesudasan <rjesudasan@leaporbit.com>
2017-02-27 09:53:02 +0000

    Deleted /Web/src/Web

--- Web/src/Web/src/app/shared/public/public.routes.ts
+++
@@ -1,9 +0,0 @@
-import{Routes,RouterModule} from '@angular/router';
-import{LoginComponent} from '../../login/login.component';
-
-export const PUBLIC_ROUTES: Routes = [    
-    {  path: '',redirectTo:'login' ,pathMatch:'full' },    
-    {  path: 'login', component: LoginComponent },    
-    //{ path: '**', redirectTo: '' }
-];
-
