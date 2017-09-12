77c0a2761d8a47f1315a78147c74e03c3c283f8e
suarabh <saurabhkumar@gwu.edu>
2017-09-12 11:47:16 -0400

    Form Data retention and validation

--- Web/src/app/case/casefield.component.ts
+++ Web/src/app/case/casefield.component.ts
@@ -7,12 +7,13 @@ import { Case } from '../shared/models/caseModel';
     templateUrl: './caseField.html'
 })
 
+
 export class CaseFieldComponent implements OnChanges {
     @Input() FieldName: string;
     @Input() ViewModel: CaseViewModel;
     @Input() form: FormGroup;
 
-    
+
 
 
     Field: TemplateField;
@@ -23,27 +24,51 @@ export class CaseFieldComponent implements OnChanges {
     constructor() {
 
     }
+
+
     ngOnChanges(ch: SimpleChanges): void {
         if (!this.ViewModel || !this.ViewModel.Template) return;
         var fields = this.ViewModel.Template.Fields;
         for (var i = 0; i < fields.length; ++i) {
             if (fields[i].Name == this.FieldName) {
                 this.Field = fields[i];
-                // debugger;
-                // console.log("it hit here" + i);
-
                 break;
+            }
+        }
+    }
+
+    //This peace of code (bindModel) does two way does template -> model data binding sice reactive forms do not support two way daat binding.
+    //On each value change, all the value from ViewModel.Data will be assigned to each form control model, making them valid. This is required for succesful validation upon hitting 'Submit" button.
+
+    bindModel() {
+        var key;
+        for (key in this.ViewModel.Data) {
+            if (this.ViewModel.Data.hasOwnProperty(key)) {
+                this.form.controls[key].setValue(this.ViewModel.Data[key]);
             }
         }
     }
 
 
     setValue(val: number | string | boolean): void {
+        //Calcaute year of death from Date of Death entered by user
+
+
 
         this.ViewModel.Data[this.FieldName] = val;
+
         this.ViewModel.changeset[this.FieldName] = val;
         this.ViewModel.OnChange();
 
+        if (this.ViewModel.Data['DateofDeath']) {
+            var dateOfDeath = this.ViewModel.Data['DateofDeath'];
+            var yearOfDeath = Number(dateOfDeath.substr(0, 4));
+            this.ViewModel.changeset['YearofDeath'] = yearOfDeath.toString();
+            this.ViewModel.Data['YearofDeath'] = yearOfDeath.toString();
+            this.ViewModel.OnChange();
+        }
+
+        //Calculare age from Date of Death and Date of Birth provided by the user
         if (this.ViewModel.Data['DateofDeath'] && this.ViewModel.Data['DateofBirth']) {
             var dateOfDeath = this.ViewModel.Data['DateofDeath'];
             var dateOfBirth = this.ViewModel.Data['DateofBirth'];
@@ -53,22 +78,33 @@ export class CaseFieldComponent implements OnChanges {
             if (yearOfDeath > yearOfBirth) {
                 var age = yearOfDeath - yearOfBirth;
                 console.log(age);
-                this.ViewModel.Data['AgeatDeath'] = age;
+                this.ViewModel.changeset['AgeatDeath'] = age.toString();
+                this.ViewModel.Data['AgeatDeath'] = age.toString();
                 //make atemplate for Age or find a way to include Age in the final data getting submitted 
             }
+            else {
+                this.ViewModel.Data['AgeatDeath'] = "Invalid Data. Please make sure Date of Death is later than Date of Birth";
+            }
         }
+        this.bindModel();
+
+
+
+
+
         // todo: trigger saves
     }
 
+
     //code to update the selection in DocumentDB
     setCheckboxValue(val: number | string): void {
+        this.bindModel();
         console.log(this.ViewModel.Data[this.FieldName]);
-        if(!this.ViewModel.Data[this.FieldName])
-            {
-                console.log(this.ViewModel.Data);
-                this.ViewModel.Data[this.FieldName].property = true;
-                //this.ViewModel.Data.JSON,parse();
-            }
+        if (!this.ViewModel.Data[this.FieldName]) {
+            console.log(this.ViewModel.Data);
+            this.ViewModel.Data[this.FieldName].property = true;
+            //this.ViewModel.Data.JSON,parse();
+        }
         var p = this.ViewModel.Data[this.FieldName];
         for (var key in p) {
             if (key === val) {
@@ -81,7 +117,7 @@ export class CaseFieldComponent implements OnChanges {
 
 
     setCheckVal(val: number | string | boolean): void {
-
+        this.bindModel();
         var p = this.ViewModel.Data[this.FieldName];
         this.ViewModel.Data[this.FieldName] = !p;
         this.ViewModel.changeset[this.FieldName] = !p;
@@ -89,16 +125,17 @@ export class CaseFieldComponent implements OnChanges {
     }
 
     setRadioVal(val: number | string | boolean): void {
-                console.log("radioNewVal" + typeof(val));
-                console.log("RadioCrrentVal" + this.ViewModel.Data[this.FieldName])
-                var currentVal = 2;
-                console.log(currentVal);
-                this.ViewModel.Data[this.FieldName] = val;
-                this.ViewModel.changeset[this.FieldName] = val;
-                this.ViewModel.OnChange();
-                // var p = this.ViewModel.Data[this.FieldName];
-                // this.ViewModel.Data[this.FieldName] = !p;
-                // this.ViewModel.changeset[this.FieldName] = !p;
-                // this.ViewModel.OnChange();
-            }
+        this.bindModel();
+        console.log("radioNewVal" + typeof (val));
+        console.log("RadioCrrentVal" + this.ViewModel.Data[this.FieldName])
+        var currentVal = 2;
+        console.log(currentVal);
+        this.ViewModel.Data[this.FieldName] = val;
+        this.ViewModel.changeset[this.FieldName] = val;
+        this.ViewModel.OnChange();
+        // var p = this.ViewModel.Data[this.FieldName];
+        // this.ViewModel.Data[this.FieldName] = !p;
+        // this.ViewModel.changeset[this.FieldName] = !p;
+        // this.ViewModel.OnChange();
+    }
 }
